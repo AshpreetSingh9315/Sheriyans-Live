ad18d3882f8d83e21202a5e066cf98d7513ea2a1
Ash Singh <Ash Singh>
2025-02-26 00:08:21 +0530

    new res Project

--- ObeysProject/script.js
+++ ObeysProject/script.js
@@ -1,13 +1,40 @@
-
-function magnet(){
-  Shery.makeMagnet(".header-right p , .header svg");
+function mouseMove(){
   document.addEventListener("mousemove", function (dets) {
     gsap.to("#crsr", {
       x: dets.x,
       y: dets.y,
-      duration: 0.1,
     });
   });
+
+  var mouseFollwer = document.querySelector(".video-container");
+  var video = document.querySelector(".video-container video");
+  var videoImg = document.querySelector(".video-container img");
+  mouseFollwer.addEventListener("mouseenter", function(){
+    gsap.to("#crsr",{
+      opacity:0,
+    })
+    mouseFollwer.addEventListener("mousemove", function(dets){
+      gsap.to(".video-btn", {
+        left: dets.x,
+        y: dets.y,
+        duration: 0.1,
+      });
+    });
+  })
+  mouseFollwer.addEventListener("mouseleave", function(){
+    gsap.to("#crsr",{
+      opacity:1,
+    })
+  })
+  mouseFollwer.addEventListener("click", function(){
+     video.play();
+     video.style.opacity = 1;
+     videoImg.style.opacity = 0;
+  })
+}
+
+function magnet() {
+  Shery.makeMagnet(".header-right p , .header svg");
 }
 
 function loadingAnimation() {
@@ -45,40 +72,73 @@ function loadingAnimation() {
     ease: "power4.in",
   });
 
-  tl.from(".subhero h1 , .now", {
-    y: 150,
-    opacity: 0,
-    duration: 0.6,
-    stagger: 0.2,
+  tl.from(
+    ".subhero h1 , .now",
+    {
+      y: 150,
+      opacity: 0,
+      duration: 0.6,
+      stagger: 0.2,
+    },
+    "-=.7"
+  );
+ 
+}
+
+function lenisScrolling() {
+  // Initialize Lenis
+  const lenis = new Lenis({
+    autoRaf: true,
+    smoothWheel: true,
+  });
+
+  // Listen for the scroll event and log the event data
+  lenis.on("scroll", (e) => {
+    console.log(e);
   });
 }
- 
-function locomotiveAnimation(){
-  gsap.registerPlugin(ScrollTrigger);
 
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-locoScroll.on("scroll", ScrollTrigger.update);
-
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, 
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
- 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-ScrollTrigger.refresh();
-
+function SheryJs() {
+  Shery.imageEffect(".imgs_container", {
+    style: 5,
+    config: {
+      a: { value: 1.6, range: [0, 30] },
+      b: { value: -0.82, range: [-1, 1] },
+      zindex: { value: "9996999", range: [-9999999, 9999999] },
+      aspect: { value: 0.7120685188137755 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: true },
+      growSize: { value: 9.98, range: [1, 15] },
+      durationOut: { value: 1.56, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: true },
+      maskVal: { value: 1.27, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 0 },
+      noise_speed: { value: 1.37, range: [0, 10] },
+      metaball: { value: 0.5, range: [0, 2] },
+      discard_threshold: { value: 0.56, range: [0, 1] },
+      antialias_threshold: { value: 0.01, range: [0, 0.1] },
+      noise_height: { value: 0.34, range: [0, 2] },
+      noise_scale: { value: 11.45, range: [0, 100] },
+    },
+    gooey: true,
+  });  
 }
 
+
+mouseMove()
 magnet();
 loadingAnimation();
-locomotiveAnimation();
+lenisScrolling();
+SheryJs();
---
+++ Responsive-4/script.js
@@ -0,0 +1,8 @@
+var navData = document.querySelector('.nav-part');
+var navBtn = document.querySelector('.nav-btn');
+var navBtnCancel = document.querySelector('.nav-btn-cancel');
+
+navBtn.addEventListener('click',function(){
+    navData.classList.toggle('show')
+})
+ 
